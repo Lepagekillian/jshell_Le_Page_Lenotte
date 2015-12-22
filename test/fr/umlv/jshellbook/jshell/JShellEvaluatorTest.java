a163e2e9783151fc5c16ec89b98a280cfd91c8fe
ErwanLenotte <erwan.lenotte@gmail.com>
2015-12-22 10:53:15 +0100

    Test pour JshellEvaluator

---
+++ test/fr/umlv/jshellbook/jshell/JShellEvaluatorTest.java
@@ -0,0 +1,17 @@
+package fr.umlv.jshellbook.jshell;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JShellEvaluatorTest {
+
+    @SuppressWarnings("static-method")
+    @Test
+    public void testEval() {
+	JShellEvaluator parser = new JShellEvaluator();
+	String res = parser.eval("System.out.println(\"test\");");
+	assertEquals("test", res);
+    }
+
+}
