794eb85fa63516fce04897dcad61b96338dd97da
Lepagekillian <killianlepage@gmail.com>
2015-12-22 16:22:03 +0100

    maj du parsage Markdown en réglant la visibilité

--- src/fr/umlv/jshellbook/markdownanalyzer/MarkdownToHTML.java
+++ src/fr/umlv/jshellbook/markdownanalyzer/MarkdownToHTML.java
@@ -2,25 +2,26 @@ package fr.umlv.jshellbook.markdownanalyzer;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.Objects;
 //import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 
 import org.pegdown.PegDownProcessor;
 
 public class MarkdownToHTML {
 
-	
-		private MarkdownToHTML() {
+	private final PegDownProcessor processor ;
 
+		public MarkdownToHTML(PegDownProcessor process) {
+			this.processor= Objects.requireNonNull(process);
 		}
 		
-		public static String parse(Path theFile) throws IOException{
-			PegDownProcessor processor = new PegDownProcessor();
+		public  String parse(Path theFile) throws IOException{
+
 			try(Stream<String> fi = Files.lines(theFile)){
 				String str = fi.collect(Collectors.joining("\n"));
-				str = processor.markdownToHtml(str);
+				str = this.processor.markdownToHtml(str);
 				return str;
 			}
 		}
--- test/fr/umlv/jshellbook/markdownanalyzer/MainTest.java
+++ test/fr/umlv/jshellbook/markdownanalyzer/MainTest.java
@@ -3,13 +3,16 @@ package fr.umlv.jshellbook.markdownanalyzer;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+import org.pegdown.PegDownProcessor;
+
 import fr.umlv.jshellbook.markdownanalyzer.MarkdownToHTML;
 
 public class MainTest {
 	
 	public static void main(String[] args) {
+		MarkdownToHTML work =  new MarkdownToHTML(new PegDownProcessor());
 		try {
-			System.out.println(MarkdownToHTML.parse(Paths.get("ressources/example_markdown")));
+			System.out.println(work.parse(Paths.get("ressources/example_markdown")));
 		} catch (IOException e) {
 			System.out.println("File not found.");
 			return;
