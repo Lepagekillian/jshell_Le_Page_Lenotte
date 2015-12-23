6ac1823468ae5522b528ac4ab8bbd81c4820c7e2
ErwanLenotte <erwan.lenotte@gmail.com>
2015-12-23 16:54:00 +0100

    Maitenant on peut éxécuter plusieurs lignes de codes...

--- src/fr/umlv/jshellbook/jshell/JShellEvaluator.java
+++ src/fr/umlv/jshellbook/jshell/JShellEvaluator.java
@@ -1,41 +1,64 @@
 package fr.umlv.jshellbook.jshell;
 
 import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jdk.jshell.JShell;
 import jdk.jshell.JShell.Builder;
+import jdk.jshell.SnippetEvent;
 
-public class JShellEvaluator {
-    
-    private final Builder builder;
-    private final PrintStream printStream;
-    private final ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
-    
-    public JShellEvaluator() {
-	this.printStream = new PrintStream(this.arrayOutputStream);
-	this.builder = JShell.builder();
-	this.builder.err(this.printStream);
-	this.builder.out(this.printStream);
-    }
+public class JShellEvaluator implements Closeable {
 
+	private final Builder builder;
+	private final JShell jShell;
 
-    public String eval(final String codeToEval){
-	Objects.requireNonNull(codeToEval);
-	try(JShell jShell = this.builder.build()){
-	    jShell.eval(codeToEval);
-	    String resEval = this.arrayOutputStream.toString();
-	    this.arrayOutputStream.reset();
-	    return resEval;
+	private final PrintStream printStream;
+	private final ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+
+	public JShellEvaluator() {
+		this.printStream = new PrintStream(this.arrayOutputStream);
+		this.builder = JShell.builder();
+		this.builder.err(this.printStream);
+		this.builder.out(this.printStream);
+		this.jShell = this.builder.build();
 	}
-    }
-    
-    
-    public static void main(String[] args) {
-	
-	JShellEvaluator parser = new JShellEvaluator();
-	String res = parser.eval("System.err.println(\"test\");");
-	System.out.println(res);
-    }
+
+	private void evalOneCodeLine(final String lineToEval) {
+		Objects.requireNonNull(lineToEval);
+		this.jShell.eval(lineToEval);
+	}
+
+	public String evalCodeLines(List<String> linesToEval) {
+		Objects.requireNonNull(linesToEval);
+
+		for (String line : linesToEval) {
+			evalOneCodeLine(line);
+		}
+		String resEval = this.arrayOutputStream.toString();
+		this.arrayOutputStream.reset();
+		return resEval;
+	}
+
+	@Override
+	public void close() throws IOException {
+		this.jShell.close();
+		this.printStream.close();
+		this.arrayOutputStream.close();
+	}
+
+	public static void main(String[] args) throws IOException {
+		try (JShellEvaluator evaluator = new JShellEvaluator()) {
+			List<String> lines = new ArrayList<>();
+			lines.add("int i =0;");
+			lines.add("while(i<100){i++;}");
+			lines.add("System.out.println(i);");
+			evaluator.evalCodeLines(lines);
+		}
+	}
+
 }
--- test/fr/umlv/jshellbook/jshell/JShellEvaluatorTest.java
+++ test/fr/umlv/jshellbook/jshell/JShellEvaluatorTest.java
@@ -2,23 +2,31 @@ package fr.umlv.jshellbook.jshell;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class JShellEvaluatorTest {
 
-    
-    @Test(expected=NullPointerException.class)
-    public void testNull(){
-	JShellEvaluator shellEvaluator  = new JShellEvaluator();
-	shellEvaluator.eval(null);
-    }
-    
-    
-    @Test
-    public void testEval() {
-	JShellEvaluator parser = new JShellEvaluator();
-	String res = parser.eval("System.out.println(\"test\");");
-	assertEquals("test\n", res);
-    }
+	@SuppressWarnings("static-method")
+	@Test(expected = NullPointerException.class)
+	public void testNull() throws IOException {
+		try (JShellEvaluator shellEvaluator = new JShellEvaluator()) {
+			shellEvaluator.evalCodeLines(null);
+		}
+	}
+
+	@Test
+	@SuppressWarnings("static-method")
+	public void testEval() throws IOException {
+		try (JShellEvaluator shellEvaluator = new JShellEvaluator()) {
+			List<String> lines = new ArrayList<>();
+			lines.add("System.out.println(\"test\");");
+			String res = shellEvaluator.evalCodeLines(lines);
+			assertEquals("test\n", res);
+		}
+	}
 
 }
