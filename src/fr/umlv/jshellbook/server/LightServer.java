723f2c2fa0e446bdb7394c9808e553f0c167e818
Lepagekillian <killianlepage@gmail.com>
2015-12-28 16:16:35 +0100

    LightServer à corriger. Killian

--- src/fr/umlv/jshellbook/server/LightServer.java
+++ src/fr/umlv/jshellbook/server/LightServer.java
@@ -8,10 +8,10 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.file.FileSystem;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.handler.StaticHandler;
 
 public class LightServer extends AbstractVerticle {
 	private final Path workingDirectory;
@@ -30,16 +30,25 @@ public class LightServer extends AbstractVerticle {
 				.sendFile("./javascript/communication_server_test.html");
 	}
 
+
+
 	private void readingDirectoryRoutine(RoutingContext routingContext) {
-		//System.out.println(".*\\.\\(mkdown\\)" );
+		String normalisedPath = routingContext.normalisedPath();
 		System.out.println(routingContext.normalisedPath());
-		System.out.println("hello");
-		List<String> toTreat = routingContext.vertx().fileSystem()
-				.readDirBlocking(this.workingDirectory.toString()+routingContext.normalisedPath());
-		routingContext.response().end(toTreat.toString());
-		
-	
+		FileSystem fs = routingContext.vertx().fileSystem();
+		routingContext.response().setChunked(true);
+		routingContext.response().putHeader("content-type", "text/html");
+
+		List<String> toTreat = fs.readDirBlocking(
+				this.workingDirectory.toString() + normalisedPath, "[^.]*");
+		for (String line : toTreat) {
+			routingContext.response().write(
+					"<a href =\" " + line + "\">" + line);
+			routingContext.response().write("<br>");
+		}
+		routingContext.response().end();
 	}
+
 
 	private static Handler<AsyncResult<HttpServer>> futureTreatment(
 			Future<Void> fut) {
@@ -57,9 +66,7 @@ public class LightServer extends AbstractVerticle {
 	public void start(Future<Void> fut) {
 		Router router = Router.router(this.vertx);
 		router.get("/jsRoom").handler(this::jsRoutine);
-		router.get("/*.mkdown").handler(this::readingDirectoryRoutine);
-		router.route().handler(StaticHandler.create(this.workingDirectory.toString())
-		.setDirectoryListing(true).setIncludeHidden(false));
+		router.route().handler(this::readingDirectoryRoutine);
 		router.post().handler(this::postRoutine);
 		this.vertx.createHttpServer().requestHandler(router::accept)
 				.listen(8989, futureTreatment(fut));
