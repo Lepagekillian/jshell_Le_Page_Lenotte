8635957a60f79f88e356a6de3b50b466877deb07
ErwanLenotte <erwan.lenotte@gmail.com>
2015-12-28 23:46:04 +0100

    Modif pour JSON

--- javascript/myCtrl.js
+++ javascript/myCtrl.js
@@ -1,4 +1,12 @@
 app.controller("myCtrl", function($scope) {
-    $scope.firstName	= "John";
-    $scope.lastName= "Doe";
+$http({
+  method: 'GET',
+  url: 'http://localhost:8989'
+}).then(function successCallback(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
 });
