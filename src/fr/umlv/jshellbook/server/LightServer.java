f24794280bb7aaae120525c8f71f69d1a43b3d41
Lepagekillian <killianlepage@gmail.com>
2015-12-29 19:00:36 +0100

    LightServer màj pour permettre récup de l'id d'un exo. Killian

--- src/fr/umlv/jshellbook/server/LightServer.java
+++ src/fr/umlv/jshellbook/server/LightServer.java
@@ -14,7 +14,6 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.file.FileSystem;
 import io.vertx.core.http.HttpServer;
-import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
@@ -22,42 +21,44 @@ import io.vertx.ext.web.RoutingContext;
 public class LightServer extends AbstractVerticle {
 	private final Path workingDirectory;
 
+	/**
+	 * Construct a server which will observe into a directory and makes some
+	 * action depending of the events
+	 * 
+	 * @param path
+	 */
 	public LightServer(Path path) {
 		this.workingDirectory = Objects.requireNonNull(path);
 	}
 
-	private void postRoutine(RoutingContext routingContext) {
-		System.out.println("HELLO post/submit method :)");
-		routingContext.response().end();
+	/* Tools methods */
+
+	private void initializeRouter(Router router) {
+		router.get("/").handler(this::listLinksRoutine);
+		router.get("/:id").handler(this::helloIdRoutine);
+		router.route().handler(this::readingDirectoryRoutine);
+		router.post().handler(this::postRoutine);
 	}
 
-	private void jsRoutine(RoutingContext routingContext) {
-		routingContext.response().setChunked(true);
-		FileSystem fs = routingContext.vertx().fileSystem();
-		List<String> toTreat = fs.readDirBlocking(this.workingDirectory.toString(), "[^.]*.mkdown");
+	private StringBuilder linksIntoThePage(FileSystem fs) {
+		StringBuilder allLinks = new StringBuilder();
+		List<String> toTreat = fs.readDirBlocking(
+				this.workingDirectory.toString(), "[^.]*.mkdown");
 		List<JsonObject> jsonObjects = new ArrayList<>();
 		for (String line : toTreat) {
 			jsonObjects.add(LightServer.makeExoJson(line));
 		}
-		routingContext.response().write(Json.encode(jsonObjects));
-		routingContext.response().end();
+		linkConstructor(allLinks, jsonObjects);
+		return allLinks;
 	}
 
-	private void readingDirectoryRoutine(RoutingContext routingContext) {
-		routingContext.response().setChunked(true);
-		routingContext.response().sendFile("webroot/index.html");
-
-	}
-
-	private static Handler<AsyncResult<HttpServer>> futureTreatment(Future<Void> fut) {
-		return result -> {
-			if (result.succeeded()) {
-				System.out.println("You can work on : http://localhost:8989/");
-				fut.complete();
-			} else {
-				fut.fail(result.cause());
-			}
-		};
+	private static void linkConstructor(StringBuilder allLinks,
+			List<JsonObject> jsonObjects) {
+		for (JsonObject joo : jsonObjects) {
+			allLinks.append("<a href = \"http://localhost:8989/")
+					.append(joo.getString("id")).append("\">")
+					.append(joo.getString("id")).append("<br>");
+		}
 	}
 
 	private static JsonObject makeExoJson(String line) {
@@ -69,17 +70,53 @@ public class LightServer extends AbstractVerticle {
 			id = id.substring(0, id.indexOf('.'));
 			jsonObject.put("id", id);
 		}
-
 		return jsonObject;
+	}
+
+	/* Routine's method to use with our Server */
+
+	private void postRoutine(RoutingContext routingContext) {
+		System.out.println("HELLO post/submit method :)");
+		routingContext.response().end();
+	}
+
+	private void helloIdRoutine(RoutingContext routingContext) {
+		String myId = routingContext.request().getParam("id");
+		routingContext.response().end("<h1>Hello from : " + myId + "</h1>");
+	}
+
+	private void listLinksRoutine(RoutingContext routingContext) {
+		routingContext.response().setChunked(true);
+		FileSystem fs = routingContext.vertx().fileSystem();
+		StringBuilder allLinks = linksIntoThePage(fs);
+		routingContext.response().write(allLinks.toString());
+		routingContext.response().end();
+	}
+
+	private void readingDirectoryRoutine(RoutingContext routingContext) {
+		routingContext.response().setChunked(true);
+		routingContext.response().sendFile("webroot/index.html");
+
+	}
+
+	private static Handler<AsyncResult<HttpServer>> futureTreatment(
+			Future<Void> fut) {
+		return result -> {
+			if (result.succeeded()) {
+				System.out.println("You can work on : http://localhost:8989/");
+				fut.complete();
+			} else {
+				fut.fail(result.cause());
+			}
+		};
 	}
 
 	@Override
 	public void start(Future<Void> fut) {
 		Router router = Router.router(this.vertx);
-		router.get("/list").handler(this::jsRoutine);
-		router.route().handler(this::readingDirectoryRoutine);
-		router.post().handler(this::postRoutine);
-		this.vertx.createHttpServer().requestHandler(router::accept).listen(8989, futureTreatment(fut));
+		initializeRouter(router);
+		this.vertx.createHttpServer().requestHandler(router::accept)
+				.listen(8989, futureTreatment(fut));
 	}
 
 }
