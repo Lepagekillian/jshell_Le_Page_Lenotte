97668031de6f4dd10156e90f19e8c89d95cc2932
ErwanLenotte <erwan.lenotte@gmail.com>
2015-12-29 19:15:07 +0100

    No need to pass a pegdown

--- test/fr/umlv/jshellbook/markdownanalyzer/MainTest.java
+++ test/fr/umlv/jshellbook/markdownanalyzer/MainTest.java
@@ -10,7 +10,7 @@ import fr.umlv.jshellbook.markdownanalyzer.MarkdownToHTML;
 public class MainTest {
 	
 	public static void main(String[] args) {
-		MarkdownToHTML work =  new MarkdownToHTML(new PegDownProcessor());
+		MarkdownToHTML work =  new MarkdownToHTML();
 		try {
 			System.out.println(work.parse(Paths.get("ressources/exercice1.mkdown")));
 		} catch (IOException e) {
